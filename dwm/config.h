b1c172e94689fd0f49f200dbe6491477b0662579
gsvera-sudo <gabriel.tssvera@gmail.com>
2026-05-11 18:24:54 -0300

    novo script desktop

--- dwm/config.h
+++ dwm/config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_desktop", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "fish", NULL };
 static const char *clipcmd[] = { "/home/zup/.local/bin/screenshot", NULL };
 static const char *brupcmd[]  = { "brightnessctl", "set", "5%+", NULL };
